072668bb3f4cb832b9e84dedebce5e949b639b74
Nikhil Kilivayil <nikhilkilivayil@gmail.com>
2015-09-20 21:30:40 +0530

    Added launcher Icon. Splash screen icons and text added.

--- app/src/main/java/hsm/yengg/home/MainActivity.java
+++ app/src/main/java/hsm/yengg/home/MainActivity.java
@@ -1,10 +1,8 @@
 package hsm.yengg.home;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 import hsm.yengg.util.SessionController;
 
@@ -13,7 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SessionController session = new SessionController(this);
-        if (session.isFirstTimeRunning()) {
+        //It will show splash screen if the condition is true. It will show only first time if it work based on 'session' object,
+       /* if (session.isFirstTimeRunning()) {*/
+        if(true){
             Intent splash = new Intent(MainActivity.this, SplashActivity.class);
             startActivity(splash);
             finish();
--- app/src/main/java/hsm/yengg/home/SplashActivity.java
+++ app/src/main/java/hsm/yengg/home/SplashActivity.java
@@ -1,16 +1,44 @@
 package hsm.yengg.home;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        int mUIFlag = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+        getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 
     @Override
