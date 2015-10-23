59615e23d838f83b29b349dcf2882a4dd4e7b1ee
sibi <sibiklpm@gmail.com>
2015-10-23 17:22:56 +0530

    Recyclerview removed from Navigation Drawer and added Navigationview and ExpandableListView. Changed all icon images. Added childclick listners for expandablelistview.

---
+++ app/src/main/java/hsm/yengg/home/ExpandableListAdapter.java
@@ -0,0 +1,137 @@
+package hsm.yengg.home;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by sibi on 23/10/15.
+ */
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private Context mContext;
+    private List<ExpandedMenuModel> mListDataHeader; // header titles
+
+    // child data in format of header title, child title
+    private HashMap<ExpandedMenuModel, List<String>> mListDataChild;
+    ExpandableListView expandList;
+    public ExpandableListAdapter(Context context, List<ExpandedMenuModel> listDataHeader,HashMap<ExpandedMenuModel, List<String>> listChildData,ExpandableListView mView)
+    {
+        this.mContext = context;
+        this.mListDataHeader = listDataHeader;
+        this.mListDataChild = listChildData;
+        this.expandList=mView;
+    }
+
+    @Override
+    public int getGroupCount() {
+        int i= mListDataHeader.size();
+        Log.d("GROUPCOUNT", String.valueOf(i));
+        return this.mListDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        int childCount=0;
+        Log.e("grouppos",Integer.toString(groupPosition));
+        if(groupPosition==2)
+        {
+            childCount=this.mListDataChild.get(this.mListDataHeader.get(groupPosition))
+                    .size();
+        }
+        return childCount;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.mListDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        Log.d("CHILD",mListDataChild.get(this.mListDataHeader.get(groupPosition))
+                .get(childPosition).toString());
+        return this.mListDataChild.get(this.mListDataHeader.get(groupPosition))
+                .get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View   convertView, ViewGroup parent) {
+        ExpandedMenuModel headerTitle = (ExpandedMenuModel) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.listheader, null);
+        }
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.submenu);
+        ImageView headerIcon=    (ImageView)convertView.findViewById(R.id.iconimage);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle.getIconName());
+        headerIcon.setImageResource(headerTitle.getIconImg());
+        ImageView iconExpand = (ImageView) convertView.findViewById(R.id.icon_expand);
+        ImageView iconCollapse = (ImageView) convertView.findViewById(R.id.icon_collapse);
+
+        if (isExpanded) {
+            iconExpand.setVisibility(View.GONE);
+            iconCollapse.setVisibility(View.VISIBLE);
+        } else {
+            iconExpand.setVisibility(View.VISIBLE);
+            iconCollapse.setVisibility(View.GONE);
+        }
+
+        if (getChildrenCount(groupPosition) == 0) {
+            iconExpand.setVisibility(View.GONE);
+            iconCollapse.setVisibility(View.GONE);
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition,  boolean isLastChild, View convertView, ViewGroup parent) {
+        final String childText = (String) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_submenu, null);
+        }
+
+        TextView txtListChild = (TextView) convertView
+                .findViewById(R.id.submenu);
+
+        txtListChild.setText(childText);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
