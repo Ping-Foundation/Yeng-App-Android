2f28138f2603d4907b688a5ec1abdbfb4adcd3ce
arunkrishna007 <arunkrishna007arun@gmail.com>
2016-06-28 19:31:01 -0700

    added html file

--- app/src/main/java/hsm/yeng/rules/BtechFragment.java
+++ app/src/main/java/hsm/yeng/rules/BtechFragment.java
@@ -1,6 +1,7 @@
 package hsm.yeng.rules;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -23,13 +26,15 @@ TextView mAdmissionRules;
     TextView mEligibilityRules;
     TextView mFeechargedRules;
     TextView mDisciplineRules;
-    View view;
+    View viewstart;
     Util util;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-
-
+    LayoutInflater inflat;
+    LinearLayout lay;
+    ViewGroup contain;
+    WebView rules;
     public BtechFragment() {
         // Required empty public constructor
 
@@ -43,28 +48,53 @@ TextView mAdmissionRules;
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        Log.e("dasdas", "btech");
-
-        final ProgressDialog pDialog = new ProgressDialog(getActivity());
-        pDialog.setMessage("Loading...");
-        pDialog.show();
-
-               pDialog.dismiss();
-                view =inflater.inflate(R.layout.fragment_btech, container, false);
+        /*ProgressDialog dialog=new ProgressDialog(getActivity(),1);*/
 
 
-
+         viewstart=inflater.inflate(R.layout.fragment_btech, container, false);
+      rules= (WebView) viewstart.findViewById(R.id.webview);
+        rules.getSettings().setJavaScriptEnabled(true);
+        rules.loadUrl("file:///android_asset/rules.html");
+        rules.setVerticalScrollBarEnabled(true);
+        return viewstart;
 
 
 
 
-
-        return view;
     }
     @Override
     public void onResume() {
         Log.e("btech", "onresume");
         super.onResume();
     }
+    private class AsyncTaskRunner extends AsyncTask<View,View,View> {
 
+
+
+        @Override
+        protected View doInBackground(View... params) {
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(View view) {
+            super.onPostExecute(view);
+
+        }
+
+       /*
+                 * (non-Javadoc)
+                 *
+                 * @see android.os.AsyncTask#onPreExecute()
+                 */
+
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see android.os.AsyncTask#onProgressUpdate(Progress[])
+         */
+
+    }
 }
