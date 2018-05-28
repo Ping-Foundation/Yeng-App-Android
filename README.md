a8efb9ae6265a8fc819305c0a0b462e375d056f9
Nikhil Kilivayil <nikhilkilivayil@gmail.com>
2018-05-28 15:36:11 +0530

    Removed all contents

--- README.md
+++
@@ -1,5 +0,0 @@
-# Yeng-App-Android
-This is the Android module for Yeng App.
-Please mail to talk@crazyamigos.io for contributing this project.
-
-Main repo: https://github.com/hotshotmentors/Yeng.git
--- app/src/main/java/hsm/yeng/helper/RecyclerViewClickListener.java
+++
@@ -1,9 +0,0 @@
-package hsm.yeng.helper;
-
-import android.view.View;
-
-public interface RecyclerViewClickListener {
-    void onClick(View view, int position);
-
-    void onLongClick(View view, int position);
-}
