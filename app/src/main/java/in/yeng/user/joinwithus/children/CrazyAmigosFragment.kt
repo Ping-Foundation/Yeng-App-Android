8d9c592211a48585efa12b975cba5f2d450fc510
Prafuldev V R <thepzy64@gmail.com>
2018-06-06 13:24:59 +0530

    Completed JoinWithUsFragment with dummy content.

--- app/src/main/java/in/yeng/user/joinwithus/children/CrazyAmigosFragment.kt
+++ app/src/main/java/in/yeng/user/joinwithus/children/CrazyAmigosFragment.kt
@@ -2,11 +2,14 @@ package `in`.yeng.user.joinwithus.children
 
 import `in`.yeng.user.R
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 class CrazyAmigosFragment : Fragment() {
 
@@ -28,5 +31,19 @@ class CrazyAmigosFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val email = view.findViewById<ImageView>(R.id.email_icon)
+        val call = view.findViewById<ImageView>(R.id.call_icon)
+
+        email.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto", resources.getString(R.string.crazy_amigos_contact_email), null))
+            startActivity(Intent.createChooser(intent, "Send email..."))
+        }
+
+        call.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", resources.getString(R.string.crazy_amigos_contact_call), null))
+            startActivity(intent)
+        }
     }
 }
